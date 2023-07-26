de6b275711a7f6ecc9c5e0c20af93926bfa04eeb
Maxximusan <dragonstonecave@gmail.com>
2023-07-26 19:15:00 +0300

    7.3

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import TodoList from './components/TodoList'
+import AddTodo from './components/AddTodo';
 
 const App: React.FC = () => {
   const todos = [{id: '1', title: 'text'}]
   return (
     <div className="App">
+      <AddTodo />
       <TodoList todos={ todos } />
     </div>
   );
---
+++ src/components/AddTodo.tsx
@@ -0,0 +1,23 @@
+import React, {useRef} from 'react';
+
+const AddTodo: React.FC = () => {
+    const titleInputRef = useRef<HTMLInputElement>(null)
+
+    function submitHandler(e: React.FormEvent) {
+        e.preventDefault();
+        console.log(titleInputRef.current?.value);
+        
+}
+
+    return (
+    <form onSubmit={submitHandler}>
+      <div>
+        <span> Add Title</span>
+         <input type='text' id='add-todo' ref={ titleInputRef} />
+     </div>
+     <button type='submit'> Add Todo</button>
+    </form>
+)
+}
+
+export default AddTodo
