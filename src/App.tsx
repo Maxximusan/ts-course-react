b8a780633df08c20b975140af35317401068b4a1
Maxximusan <103362267+Maxximusan@users.noreply.github.com>
2024-06-26 18:11:44 +0300

    practicing

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import React, {useState} from 'react';
 
 import TodoList from './components/TodoList'
 import AddTodo from './components/AddTodo';
+
+import { Product } from './components/Product'
+import {products} from './data/products'
 import { IItem } from './types/todo'
 
 const App: React.FC = () => {
   const [todos, setTodos] = useState<IItem[]>([])
+  const [count, setCount] = useState(0)
 
   function todoAddHandler(todo: IItem) {
     setTodos((prevTodos) => {
@@ -26,12 +30,41 @@ const App: React.FC = () => {
     
   }
 
+
+  const Increase = () => {
+   
+    setCount(count +1)
+    console.log(count);
+    
+  }
+
+  const Decrease = () => {
+    
+    setCount(prev => prev -= 1)
+    console.log(count);
+    
+  }
+  
   return (
     <div className="App">
+      <div className='border border-purple-700'>
+      <h1> Start practicing </h1>
+      <button className='m-4 p-4 border border-green-900' onClick={Increase}> Increase!</button>
+      <button className='m-4 p-4 border border-red-700' onClick={Decrease}> Decrease!</button>
+      </div>
+      <Product prod={products[0]}/>
+      <Product prod={products[1]}/>
+
+      <div className='mt-14'>
       <AddTodo onAddTodo={ todoAddHandler} />
       <TodoList todos={todos} onRemoveTodo={ todoRemoveHandler} />
+      </div>
     </div>
   );
+  // return React.createElement('h2', {className:'wow'}, 'HELLO')
+
+
 }
+
 
 export default App;
---
+++ src/components/Product.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { IProduct } from "../types/models";
+
+interface ProductProps {
+    prod: IProduct
+}
+
+export const Product = (props: ProductProps) => {
+
+console.log(props);
+const {prod} = props
+
+const [details, setDetails] = useState(false)
+
+// this alternative variant for change btn
+const btnBgClassName = details ? 'bg-blue-400' :' bg-yellow-400' 
+const btnClasses = ['py-2 px-2 border', btnBgClassName ]
+
+
+const showHideDetails = () =>{
+    setDetails(prev => !prev)
+}
+
+
+
+    return(
+        <>
+        <h2 className="m-6 text-2xl"> Product </h2>
+        <div className="border border-r-cyan-500 py-2 px-4 rounded flex flex-col items-center mb-2">
+            <img src={prod.image} className="w-1/6" alt={prod.title} />
+        <p>{prod.title}</p>
+        <p>{prod.price}</p>
+
+        {/* this alternative variant for change btn */}
+        <button onClick={showHideDetails} className={btnClasses.join(' ')}> 
+            { details ? 'Hide Details' : 'Show Details'}
+        </button>
+
+{/* OR my variant (primary*/}
+
+{/* { details ? 
+<button onClick={showHideDetails} className="py-2 px-2 border bg-blue-400"> Hide Details</button>
+ : 
+<button onClick={showHideDetails} className="py-2 px-2 border bg-yellow-400"> Show Details</button>
+} */}
+        
+        { details && <div>
+            <p> {prod.description}</p>
+             <p>Rate: <span className="font-bold">{prod.rating.rate}</span> </p>
+        </div> }
+        </div>
+        </>
+    )
+}
